2ad48fe775beefa267f554390e9a895eaf6db930
Mohamed Amoussa <mohamedj81@hotmail.com>
2024-08-08 22:56:47 +0200

    implement inial file and folder structure

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import daisyui from "daisyui";
 
 const config: Config = {
   content: [
@@ -10,11 +11,13 @@ const config: Config = {
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
+    container: {
+      center: true,
+    },
   },
-  plugins: [],
+  plugins: [daisyui],
 };
 export default config;
